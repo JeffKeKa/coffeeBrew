07a570922a430dbf2536f4ad1e8d4fa0ff1fc73d
ALESSANDRO BENTO <alessandro_bento@estudante.sesisenai.org.br>
2024-04-10 17:35:15 -0300

    back end

--- coffeeLixeira/src/componentes/carrosel/index.jsx
+++ coffeeLixeira/src/componentes/carrosel/index.jsx
@@ -1,24 +1,45 @@
 
-import prop from "../../assets/prop.jpg"
-import bolored from "../../assets/bolored.jpg"
-
+import { useState, useEffect } from "react";
+// import bolored from "../../assets/bolored.jpg"
+import axios from "axios"
 
 import { Carousel } from "@material-tailwind/react";
 export const Carrosel = () => {
+
+  const [produtos, setProdutos] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+
+        const response = await axios.get("http://localhost:8010/coffebrew/produto/lista");
+        setProdutos(response.data);
+       // console.log(response)
+      } catch (error) {
+        console.error("Erro ao buscar dados:", error);
+      }
+    }
+    fetchData();
+  }, []);
+
   return (
-    <Carousel transition={{ duration: 1 }} className="rounded-xl  h-96" >
-      
+    // <Carousel transition={{ duration: 1 }} className="rounded-xl  h-96" >
+    <Carousel transition={{ duration: 1 }}  >
+      {produtos.map((produto) => (
+        <div key={produto.idProduto}>
+          <h1>{produto.nome}</h1>
       <img
-        src={prop}
-        alt="image 1"
+        src={`a${produto.idProduto}.jpg`}
+        alt="image"
         className="h-full w-full object-cover w-full h-96 bg-cover float-left "
       />
-    
-      <img
+      {/* <img
         src={bolored}
         alt="image 3"
         className="h-full w-full object-cover w-full h-96 bg-cover float-left"
-      />
+      /> */}
+      </div>
+      ))}
     </Carousel>
   );
 };
