e8b9bd3f59d4298c91bddfb6716eb295af96ae4d
Joaomartinsc <110736155+Joaomartinsc@users.noreply.github.com>
2024-04-25 16:31:52 -0300

    joaao

--- coffeeBrewReact/src/componentes/coffeeBrewMenu.jsx
+++ coffeeBrewReact/src/componentes/coffeeBrewMenu.jsx
@@ -1,55 +1,31 @@
-import { useState } from 'react'
-import './color.css'
-import './forme.css'
-import './letter.css'
-import CardProduto from './card/cardProduto.jsx'
-<<<<<<< HEAD
-import MenuLateral from './menuLateral/menuLateral.jsx'
+import { useState } from "react";
+import "./color.css";
+import "./forme.css";
+import "./letter.css";
+import CardProduto from "./card/cardProduto.jsx";
+import MenuLateral from "./menuLateral/menuLateral.jsx";
 
-=======
-import NaviBarMenu from './naviBarMenu/NaviBarMenu.jsx'
->>>>>>> 3fd4841c6cb577b18275f2f53a9155513d35cbc9
+import NaviBarMenu from "./naviBarMenu/NaviBarMenu.jsx";
 
 function CoffeeBrewMenu() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
-    <div className='paginaMenu'>
-
-    <MenuLateral/>
-
-    <div>
-<<<<<<< HEAD
-
-    <header className='colorCafe formeHeader'>
-      <h1>CAFÉ</h1>
-    </header>
-    <div className='menuCentral'>
-      <section >
-      <CardProduto/>
-      <CardProduto/>
+    <div className="paginaMenu">
+      <MenuLateral />
      
-    
-=======
-    <section>
-      <NaviBarMenu/>
-      <h1>ola</h1>
->>>>>>> 3fd4841c6cb577b18275f2f53a9155513d35cbc9
-    </section>
-    <section>
-      <CardProduto/>
-      <CardProduto/>
-      
-      
-    </section>
-
+      <div>
+      <NaviBarMenu />
+        <header className="colorCafe formeHeader"></header>
+        <div className="menuCentral">
+          <section>
+            <CardProduto />
+            <CardProduto />
+          </section>
+        </div>
+      </div>
     </div>
-
-    </div>
-    
-      
-    </div>
-  )
+  );
 }
 
-export default CoffeeBrewMenu
+export default CoffeeBrewMenu;
